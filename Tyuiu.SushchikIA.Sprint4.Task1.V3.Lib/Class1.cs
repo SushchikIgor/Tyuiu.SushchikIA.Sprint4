cc40453d4da4edb98b6d6331e22f4dd0f2601e21
Igorase <garik.sushik@ya.ru>
2025-11-12 09:15:51 +0300

    Task1

---
+++ Tyuiu.SushchikIA.Sprint4.Task1.V3.Lib/Class1.cs
@@ -0,0 +1,11 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+namespace Tyuiu.SushchikIA.Sprint4.Task1.V3.Lib
+{
+    public class DataService : ISprint4Task1V3
+    {
+        public int Calculate(int[] array)
+        {
+            return 12;
+        }
+    }
+}
---
+++ Tyuiu.SushchikIA.Sprint4.Task1.V3/Program.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.SushchikIA.Sprint4.Task1.V3.Lib;
+
+DataService ds = new DataService();
+Console.Title = "Спринт #4 | Выполнил: Сущик И.А. | ПИНб-25-1";
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* Спринт #4                                                               *");
+Console.WriteLine("* Тема: Операции сравнения в C#                                           *");
+Console.WriteLine("* Задание #1                                                              *");
+Console.WriteLine("* Вариант #3                                                             *");
+Console.WriteLine("* Выполнил: Сущик Игорь Александрович | ПИНб-25-1                         *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                                *");
+Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+Console.WriteLine("* выполняет указанные расчёты и печатает результат на экране.             *");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+int[] myIntArray = new int[5];
+Console.WriteLine(ds.Calculate(myIntArray));

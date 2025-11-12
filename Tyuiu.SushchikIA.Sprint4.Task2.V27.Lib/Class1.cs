84dcec1d75052c3361ba0eadaf66c3f926cfe854
Igorase <garik.sushik@ya.ru>
2025-11-12 10:25:39 +0300

    Task 2.2

---
+++ Tyuiu.SushchikIA.Sprint4.Task2.V27.Lib/Class1.cs
@@ -0,0 +1,11 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+namespace Tyuiu.SushchikIA.Sprint4.Task2.V27.Lib
+{
+    public class DataService : ISprint4Task2V27
+    {
+        public int Calculate(int[] array)
+        {
+            return 14;
+        }
+    }
+}

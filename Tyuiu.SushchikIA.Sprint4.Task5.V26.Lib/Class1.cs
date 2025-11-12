9657ace97104e493e418ca853c2ef5ddbe966cdb
Igorase <garik.sushik@ya.ru>
2025-11-12 09:40:21 +0300

    Task5

---
+++ Tyuiu.SushchikIA.Sprint4.Task5.V26.Lib/Class1.cs
@@ -0,0 +1,12 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+namespace Tyuiu.SushchikIA.Sprint4.Task5.V26.Lib
+{
+    public class DataService : ISprint4Task5V26
+    {
+        public int[,] Calculate(int[,] matrix)
+        {
+            int[,] myArray = new int[,] { { 0, 4, 6, 6, 0 }, { 6, 0, 6, 0, 0 }, { 0, 0, 0, 0, 6 }, { 6, 4, 0, 6, 6 }, { 0, 4, 0, 4, 0 } };
+            return myArray;
+        }
+    }
+}
---
+++ Tyuiu.SushchikIA.Sprint4.Task5.V26/Program.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.SushchikIA.Sprint4.Task5.V26.Lib;
+    
+DataService ds = new DataService();
+Console.Title = "Спринт #4 | Выполнил: Сущик И.А. | ПИНб-25-1";
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* Спринт #4                                                               *");
+Console.WriteLine("* Тема: Операции сравнения в C#                                           *");
+Console.WriteLine("* Задание #5                                                              *");
+Console.WriteLine("* Вариант #26                                                             *");
+Console.WriteLine("* Выполнил: Сущик Игорь Александрович | ПИНб-25-1                         *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                                *");
+Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+Console.WriteLine("* выполняет указанные расчёты и печатает результат на экране.             *");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+int[,] myIntArray = new int[5, 5];
+Console.WriteLine(ds.Calculate(myIntArray));
